26d9a8c812e037bbad103a9f7a1cf5eccceae050
faridan <suripgentayangan@gmail.com>
2023-07-16 07:48:27 +0800

    feat: add encoder stasiun

--- src/api/main.py
+++ src/api/main.py
@@ -14,20 +14,36 @@ def load_model():
             "message": str(e)
         }
         return response
+
+def load_encoder():
+    try:
+        ohe_stasiun = joblib.load("/home/muhfaridansutariya/asistensi_mlprocess/MLProcess/Asistensi/jakarta-pollution-classification/models/ohe_stasiun.pkl")
+        return ohe_stasiun
+    except Exception as e:
+        response = {
+            "status": 204,
+            "message": str(e)
+        }
+        return response
     
 @app.post("/predict")
-async def predict(DKI1: float, DKI2: float, DKI3: float, DKI4: float, DKI5: float, pm10: float, pm25: float, so2: float, co: float, o3: float, no2: float):
+async def predict(stasiun: str, pm10: float, pm25: float, so2: float, co: float, o3: float, no2: float):
 
     model = load_model()
+    encoder = load_encoder()
 
     label = ['BAIK', 'TIDAK SEHAT']
 
+    encoded_stasiun = encoder.transform(np.array(stasiun).reshape(-1, 1))
+    numerical_features = np.array([[pm10, pm25, so2, co, o3, no2]])
+
+    input_data = np.concatenate((encoded_stasiun, numerical_features), axis=1)
+
     try:
-        input_data = np.array([[DKI1, DKI2, DKI3, DKI4, DKI5, pm10, pm25, so2, co, o3, no2]])
         prediction = model.predict(input_data)
         response = {
             "status": 200,
-            "input": [DKI1, DKI2, DKI3, DKI4, DKI5, pm10, pm25, so2, co, o3, no2],
+            "input": input_data.tolist(),
             "message": label[prediction[0]]
         }
     except Exception as e:
@@ -36,3 +52,4 @@ async def predict(DKI1: float, DKI2: float, DKI3: float, DKI4: float, DKI5: floa
             "message": str(e)
         }
     return response
+
